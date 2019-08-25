a9e991ca5bbc49ec6ca57902225a5365a4f0d981
ewenb <eboisrame@outlook.com>
2019-08-25 17:14:07 +0200

    décorateur ViewChild

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
@@ -6,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice2.component.css']
 })
 export class Exercice2Component implements OnInit {
+
+  // Récupération du contenu de la référence
+  @ViewChild('data', {static: false}) public el: ElementRef;
+  
   public valeur: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.valeur = null;
   }
+
+  addText() {
+    // Affectation de la référence via le nativeElement
+    //  et transmission au template via la variable 'valeur'
+    this.valeur = this.el.nativeElement.value;
+  }
+
+  
+
+
 
 }
